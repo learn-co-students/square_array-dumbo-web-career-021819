c89244cb7f2e0a37f115d7913d551b8a845f92ea
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 03:57:10 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  new_numbers = []
+  numbers.each do |element|
+    new_number = element**2
+    new_numbers << new_number
+  end
+  return new_numbers
 end
+
+
+
